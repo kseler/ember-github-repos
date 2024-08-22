4d15f633d2a2c586e75787c50b669fc3d7ded0a6
SNIPBIT\karlo <karloseler@gmail.com>
2024-08-22 16:53:26 +0200

    branch list test

--- tests/integration/components/branch-list-test.js
+++ tests/integration/components/branch-list-test.js
@@ -1,26 +1,34 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-github-repos/tests/helpers';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | branch-list', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders branch lists', async function (assert) {
+    this.server.get('/repos/rails/rails/branches', () => {
+      return [
+        { id: 1, type: 'branch', name: 'master' },
+        { id: 2, type: 'branch', name: 'develop' },
+      ];
+    });
 
-    await render(hbs`<BranchList />`);
+    await render(hbs`<BranchList @repo="rails/rails" />`);
 
-    assert.dom().hasText('');
+    assert.dom('[data-test-branch-name="master"]').exists();
+    assert.dom('[data-test-branch-name="develop"]').exists();
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <BranchList>
-        template block text
-      </BranchList>
-    `);
+  test('it renders message node', async function (assert) {
+    this.server.get('/repos/rails/rails/branches', () => {
+      return [];
+    });
 
-    assert.dom().hasText('template block text');
+    await render(hbs`<BranchList @repo="rails/rails" />`);
+
+    assert.dom('[data-test-message]').exists();
   });
 });
