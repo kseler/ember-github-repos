0f5e49775391f9c4468d4fc0f72d4b0c8978fc04
SNIPBIT\karlo <karloseler@gmail.com>
2024-08-22 17:18:15 +0200

    cleanup app adapter test

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -4,7 +4,6 @@ import { module, test } from 'qunit';
 module('Unit | Adapter | application', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function (assert) {
     const adapter = this.owner.lookup('adapter:application');
     assert.ok(adapter, 'adapter exists');
